805c1fade399c0cce05d7561876215e009f2b855
Chase Covington <mchasecov@gmail.com>
2022-02-02 13:39:15 -0500

    feat(db) race model validation TTD

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,2 +1,17 @@
+#=======================================|GAME TABLE SCHEMA|=======================================
+#
+# table name: races
+#
+# id                      :bigint       null: false, primary key
+# name                    :string
+# description             :text
+# health_modifier `       :integer
+# cost_modifier           :integer
+# resource_modifier       :integer`
+#
+
 class Race < ApplicationRecord
+  validates_presence_of :name, :description, :health_modifier, :cost_modifier, :resource_modifier
+  validates :health_modifier, :cost_modifier, :resource_modifier, numericality: { in: -10..10 }
+  validates :name, uniqueness: true
 end
--- spec/models/race_spec.rb
+++ spec/models/race_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Race, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(name: 'Dwarf',
+                        description: "Dwarves are skilled warriors with an affinity for stone and metal work.
+                        Racial bonuses:
+                        Your hardy nature raises your total life by 5 points!",
+                        health_modifier: 5,
+                        cost_modifier: 0,
+                        resource_modifier: 0)
+  end
+  it { should validate_uniqueness_of(:name) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a health modifier' do
+    subject.health_modifier = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a cost modifier' do
+    subject.cost_modifier = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a resource modifier' do
+    subject.resource_modifier = nil
+    expect(subject).to_not be_valid
+  end
+  it 'cannot have modifiers exceeding 10' do
+    subject.health_modifier = 11
+    subject.cost_modifier = 11
+    subject.resource_modifier = 11
+    expect(subject).to_not be_valid
+  end
+  it 'cannot have modifiers below -10' do
+    subject.health_modifier = -11
+    subject.cost_modifier = -11
+    subject.resource_modifier = -11
+    expect(subject).to_not be_valid
+  end
 end
