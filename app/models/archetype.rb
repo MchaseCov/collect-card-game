bcbdf90a0242d2da4bfee93059ed0a972c97f9c5
Chase Covington <mchasecov@gmail.com>
2022-02-02 13:46:52 -0500

    feat(db) archetype model validation TTD

--- app/models/archetype.rb
+++ app/models/archetype.rb
@@ -1,2 +1,13 @@
+#=======================================|ARCHETYPE TABLE SCHEMA|=======================================
+#
+# table name: races
+#
+# id                      :bigint       null: false, primary key
+# name                    :string
+# description             :text
+# resource_type           :string
+#
 class Archetype < ApplicationRecord
+  validates_presence_of :name, :description, :resource_type
+  validates :resource_type, inclusion: { in: %w[mana energy hybrid] }
 end
--- spec/models/archetype_spec.rb
+++ spec/models/archetype_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Archetype, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(name: 'Barbarian',
+                        description: 'A fierce warrior with a rage for battle',
+                        resource_type: 'energy')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a description' do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+  it 'is not valid without a resource type' do
+    subject.resource_type = nil
+    expect(subject).to_not be_valid
+  end
+  it 'can have a resource of mana' do
+    subject.resource_type = 'mana'
+    expect(subject).to be_valid
+  end
+  it 'can have a resource of energy' do
+    subject.resource_type = 'energy'
+    expect(subject).to be_valid
+  end
+  it 'can have a resource of hybrid' do
+    subject.resource_type = 'hybrid'
+    expect(subject).to be_valid
+  end
+  it 'cannot have other resource type names' do
+    subject.resource_type = 'magic'
+    expect(subject).to_not be_valid
+  end
 end
