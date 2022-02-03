d3d782fe09ea6e7d6943dfbb454b99d000fe1f08
Chase Covington <mchasecov@gmail.com>
2022-02-03 11:53:57 -0500

    feat(db) account deck model TTD validation

--- app/models/account_deck.rb
+++ app/models/account_deck.rb
@@ -1,4 +1,18 @@
+#=======================================|ACCOUNT DECK TABLE SCHEMA|=======================================
+#
+# table name: account_decks
+#
+# id                      :bigint       null: false, primary key
+# name                    :string
+# user_id                 :bigint       null: false, foreign key of User table
+# card_count              :integer
+# archetype_id            :bigint       null: false, foreign key of Archetype table
+# race_id                 :bigint       null: false, foreign key of Race table
+#
 class AccountDeck < ApplicationRecord
+  validates_presence_of :name
+  validates :card_count, numericality: { in: 0..30 }
+
   has_and_belongs_to_many :party_card_parents
   belongs_to :user
   belongs_to :archetype
