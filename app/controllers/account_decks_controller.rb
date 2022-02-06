29e13b8d3ad921f0712e4398ed61b9b890980e01
Chase Covington <mchasecov@gmail.com>
2022-02-06 16:08:26 -0500

    feat: new deck basic form

--- app/controllers/account_decks_controller.rb
+++ app/controllers/account_decks_controller.rb
@@ -1,19 +1,30 @@
 class AccountDecksController < ApplicationController
-  def index
-  end
+  def index; end
 
   def new
+    @races = Race.all
+    @archetypes = Archetype.where.not(name: 'Neutral').all
+    @account_deck = AccountDeck.new
   end
 
   def create
+    @account_deck = AccountDeck.new(account_deck_params)
+    if @account_deck.save
+      redirect_to @account_deck
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def edit
-  end
+  def edit; end
 
-  def update
-  end
+  def update; end
 
-  def destroy
+  def destroy; end
+
+  private
+
+  def account_deck_params
+    params.require(:account_deck).permit(:name, :race_id, :archetype_id)
   end
 end
