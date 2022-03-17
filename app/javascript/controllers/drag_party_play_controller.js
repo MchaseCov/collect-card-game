d84ab41472e34f8b7e6d5ae6ddefb97ca7666bd5
Chase Covington <mchasecov@gmail.com>
2022-03-17 13:07:22 -0400

    rf: begin creating parent for drag play

--- app/javascript/controllers/drag_party_play_controller.js
+++ app/javascript/controllers/drag_party_play_controller.js
@@ -1,54 +1,21 @@
-import { Controller } from '@hotwired/stimulus';
-
+//import { Controller } from '@hotwired/stimulus';
+import { PlayParentController } from './play_parent_controller';
 // Connects to data-controller="drag-party-play"
-export default class extends Controller {
+export default class extends PlayParentController {
   static targets = ['playableCard', 'boardSpace', 'friendlyCardInBattle'];
 
-  static values = { playerCost: Number };
-
-  async initialize() {
+  initialize() {
+    super.initialize(this.playableCardTargets)
     // If there are no cards in play, make the play area very wide
     if (this.boardSpaceTargets.length === 1) {
       this.boardSpaceTarget.style.width = '100%';
-      this.hasNoCardsOnBoard = true;
-    }
-    this.playerCanAct = (this.element.dataset.currentTurn === this.element.dataset.playerTurn);
-  }
-
-  playableCardTargetConnected(element) {
-    if (element.dataset.cost > this.playerCostValue || this.boardSpaceTargets.length === 8 || !this.playerCanAct) {
-      this.removeDragFromElement(element);
+      this.keepBoardSpaceWide = true;
     }
   }
 
   dragStart(event) {
-    if ((event.target.getAttribute('draggable')) === 'false') {
-      this.errorFeedback(event.target);
-      event.preventDefault();
-    } else {
-      this.dragSrcEl = event.target;
-      this.boardSpaceTargets.forEach((el) => { el.classList.add('w-5', 'h-52', 'bg-clip-padding', 'px-5', '-mx-2.5'); });
-      event.target.classList.add('shadow-2xl', 'shadow-lime-500');
-      event.dataTransfer.effectAllowed = 'move';
-      event.dataTransfer.setData('text/html', event.target.innerHTML);
-    }
-  }
-
-  dragEnter(event) {
-    if (event.preventDefault) event.preventDefault();
-  }
-
-  dragOver(event) {
-    // Makes area lime. If not the "wide default area", make the hover region wide for visual
-    event.target.classList.add('bg-lime-500');
-    if (!this.hasNoCardsOnBoard) event.target.style.width = '9rem';
-    if (event.preventDefault) event.preventDefault();
-  }
-
-  dragLeave(event) {
-    // Remove lime color. Return to small size
-    event.target.classList.remove('bg-lime-500');
-    if (!this.hasNoCardsOnBoard) event.target.style.width = '0rem';
+    super.dragStart(event)
+    this.boardSpaceTargets.forEach((el) => { el.classList.add('w-5', 'h-52', 'bg-clip-padding', 'px-5', '-mx-2.5'); });
   }
 
   drop(event) {
@@ -61,25 +28,12 @@ export default class extends Controller {
   }
 
   dragEnd() {
-    // Remove all shadows from friendly cards and then enemy cards
-    if (this.dragSrcEl) {
-      this.dragSrcEl.classList.remove('shadow-2xl', 'shadow-lime-500');
-      this.boardSpaceTargets.forEach((el) => el.classList.remove('w-5', 'h-52', 'bg-clip-padding', 'px-5', '-mx-2.5'));
-    }
-  }
-
-  errorFeedback(target) {
-    target.classList.add('shake');
-    target.onanimationend = () => target.classList.remove('shake');
-  }
-
-  removeDragFromElement(element) {
-    element.classList.remove('ring');
-    element.setAttribute('draggable', false);
+    super.dragEnd()
+    this.boardSpaceTargets.forEach((el) => el.classList.remove('w-5', 'h-52', 'bg-clip-padding', 'px-5', '-mx-2.5'));
   }
 
   // Target argument is passed from battlecry select controller when applicable
-  postToPlayCardPath(target = false) {
+  postToPlayCardPath(target = '') {
     fetch(`/games/${this.element.dataset.game}/play_card/party`, {
       method: 'POST',
       credentials: 'same-origin',
@@ -94,9 +48,5 @@ export default class extends Controller {
         battlecry_target: target,
       }),
     });
-  }
-
-  get battlecrySelectController() {
-    return this.application.getControllerForElementAndIdentifier(this.element, 'battlecry-select');
   }
 }
---
+++ app/javascript/controllers/play_parent_controller.js
@@ -0,0 +1,59 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="play-parent"
+export class PlayParentController extends Controller { 
+
+  playerCanAct = (this.element.dataset.currentTurn === this.element.dataset.playerTurn);
+  playerCostValue = this.element.dataset.cost
+
+  initialize(elements){
+    if(!this.playerCanAct || this.boardSpaceTargets.length >= 8) {
+      elements.forEach((el)=>this.removeDragFromElement(el))
+   } else {
+     elements.forEach((el)=>this.evaluatePlaybility(el))
+   }
+  }
+
+  removeDragFromElement(element) {
+    element.dataset.action = element.dataset.action.replace('#dragStart', '#errorFeedback')
+    element.classList.remove('ring');
+    element.setAttribute('draggable', false);
+  }
+
+  evaluatePlaybility(element){
+    if(element.dataset.cost > this[`player${element.dataset.resource}Value`]) this.removeDragFromElement(element)
+  }
+
+  errorFeedback(event) {
+    event.target.classList.add('shake');
+    event.target.onanimationend = () => event.target.classList.remove('shake');
+  }
+
+  dragStart(event) {
+    this.dragSrcEl = event.target;
+    event.target.classList.add('shadow-2xl', 'shadow-lime-500');
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', event.target.innerHTML);
+  }
+
+  dragEnter(event) {
+    if (event.preventDefault) event.preventDefault();
+  }
+  
+  dragOver(event) {
+    event.target.classList.add('bg-lime-500');
+    if (!this.keepBoardSpaceWide) event.target.style.width = '9rem';
+    if (event.preventDefault) event.preventDefault();
+  }
+
+  dragLeave(event) {
+    event.target.classList.remove('bg-lime-500');
+    if (!this.keepBoardSpaceWide) event.target.style.width = '0rem';
+  }
+
+  dragEnd() {
+    if (this.dragSrcEl) {
+      this.dragSrcEl.classList.remove('shadow-2xl', 'shadow-lime-500');
+    }
+  }
+}
