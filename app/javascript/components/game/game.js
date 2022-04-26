2f831dd68424229638d384271bbb3b1a18894e60
Chase Covington <mchasecov@gmail.com>
2022-04-26 14:36:13 -0400

    resize game table

--- app/javascript/components/game/game.js
+++ app/javascript/components/game/game.js
@@ -27,21 +27,23 @@ const Game = forwardRef((props, ref) => {
   useEffect(() => { turnButton.current.disabled = !isPlayersTurn })
 
   return html`
-<canvas id="drawContainer" data-line-drawer-target="canvas" width="0" height="0" className='fixed z-50 pointer-events-none select-none'></canvas>
-<${Sidebar} ref=${turnButton} gameInformationData=${gameInformationData} opponentInformationData=${opponentInformationData} playerInformationData=${playerInformationData} opponentCardData=${opponentCardData} playerCardData=${playerCardData} gameCurrentTurn=${gameCurrentTurn}/>
-<article id="main-game-board"
-        className="flex flex-col content-between justify-between h-full items-between flex-nowrap gap-y-6 xl:gap-y-2 game-perspective-3d overflow-clip bg-light-brown"
-        data-controller="gameplay-drag style-cards gameboard-animations"
-        data-game=${gameInformationData.id}
-        data-gameboard-animations-status-value=${gameInformationData.status}
-        data-gameboard-animations-turn-value=${gameInformationData.turn_time}
-        key="game-board"
-        ref=${(el) => thisGameReference.current.gameBoardParent = el}
-        >
+  <canvas id="drawContainer" data-line-drawer-target="canvas" width="0" height="0" className='fixed bottom-0 left-0 right-0 z-50 mx-auto pointer-events-none select-none bg-rose-500 opacity-40'></canvas>
+  <div id="inner-game-container" className="absolute bottom-0 w-full max-w-[1920px] h-full h-[123.5vh] mx-auto right-0 left-0 my-auto children-in-game-perspective">
+  <${Sidebar} ref=${turnButton} gameInformationData=${gameInformationData} opponentInformationData=${opponentInformationData} playerInformationData=${playerInformationData} opponentCardData=${opponentCardData} playerCardData=${playerCardData} gameCurrentTurn=${gameCurrentTurn}/>
+  <article id="main-game-board"
+  className="flex flex-col content-between justify-between w-full h-full max-h-[1600px] mx-auto items-between flex-nowrap gap-y-6 xl:gap-y-2 game-perspective-3d overflow-clip bg-light-brown"
+  data-controller="gameplay-drag style-cards gameboard-animations"
+  data-game=${gameInformationData.id}
+  data-gameboard-animations-status-value=${gameInformationData.status}
+  data-gameboard-animations-turn-value=${gameInformationData.turn_time}
+  key="game-board"
+  ref=${(el) => thisGameReference.current.gameBoardParent = el}
+  >
   <${createOpponentPlayerInfo} key="opInfoRegion" ref=${opponentCardInHandReference} gameData=${gameData.opponent}/>
   <${createBattlefield} key="battlefieldRegion" ref=${cardInBattleReference} friendlyCards=${playerCardData.in_battlefield} opponentCards=${opponentCardData.in_battlefield} lastPlayedCard=${gameData.lastPlayedCard}/>
   <${createFriendlyPlayerInfo} key="fpInfoRegion" ref=${friendlyCardInHandReference} gameData=${gameData.player}/>
 </article>
+</div>
 `;
 })
 
