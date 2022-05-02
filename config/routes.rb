abbffd9a8b9b58720294d29c0d09cc28952147a8
Chase Covington <mchasecov@gmail.com>
2022-05-02 10:51:38 -0400

    fix: turbo devise compatibility

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,6 @@ Rails.application.routes.draw do
   resources :archetypes, only: [:index]
   resources :races, only: [:index]
 
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
